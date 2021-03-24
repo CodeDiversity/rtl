e2f90827e695252f36480eeeaab3aca32d63a31f
Michael Trew <mtrew@wsgc.com>
2021-03-24 10:50:40 -0400

    code app.js to make tests pass tdd complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [buttonColor, setButtonColor] = useState("red");
+  const newButtonColor = buttonColor === "red" ? "blue" : "red";
   return (
     <div className="App">
-      <button style={{backgroundColor:'red'}}>Change to blue</button>
+      <button onClick={() => setButtonColor(newButtonColor)} style={{ backgroundColor: buttonColor }}>
+        Change to {newButtonColor}
+      </button>
     </div>
   );
 }
