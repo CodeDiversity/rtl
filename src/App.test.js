b7c80dbf59929aaf3d4c445012705853faa6ca37
Michael Trew <mtrew@wsgc.com>
2021-03-24 09:20:02 -0400

    updates to getByRole and adds resources

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,9 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText('Learn React');
+  const linkElement = screen.getByRole('link', {name:/learn react/i});
   expect(linkElement).toBeInTheDocument();
 });
+
+//https://www.w3.org/TR/wai-aria/#role_definitions role definitions W3C
+// https://testing-library.com/docs/queries/about/#priority which query RTL
